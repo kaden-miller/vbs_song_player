a19ba5ceaab04d99e12d168743ed5e746202ba53
Kaden <hello@coding.pizza>
2024-06-03 08:22:57 -0500

    chore: Remove unused webpack hot-update files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d9bc5f3c23e00193"; }
+/******/ 		__webpack_require__.h = function() { return "dfa5c610ad217be9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
